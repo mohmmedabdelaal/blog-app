09ac8c742ccdfb9a1e3e9e334eeb2e6f0d2d17be
Moelaal <medo.elaal@gmail.com>
2022-07-18 05:59:29 +0200

    using lodash with lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonpalceholder from '../api/jsonpalceholder';
 import { FETCH_POSTS, FETCH_USER } from '../types';
 
@@ -6,7 +7,14 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: FETCH_POSTS, payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonpalceholder.get(`/users/${id}`);
-  dispatch({ type: FETCH_USER, payload: response.data });
-};
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonpalceholder.get(`/users/${id}`);
+//   dispatch({ type: FETCH_USER, payload: response.data });
+// };
+
+export const fetchUser = _.memoize(function (id) {
+  return async function (dispatch) {
+    const response = await jsonpalceholder.get(`/users/${id}`);
+    dispatch({ type: FETCH_USER, payload: response.data });
+  };
+});
